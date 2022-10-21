2350ceb994752ff6ecabda1d59308434a94fa4e3
Mikel <mmiras004@ikasle.ehu.eus>
2022-10-21 02:39:23 +0200

    Formación

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./Pc.css"
 import "./Mobile.css"
 import MainButton from "./MainButton";
 import {BsCodeSlash} from "react-icons/bs"
-import {AiOutlineTwitter, AiOutlineClose, AiOutlineInstagram, AiFillGithub, AiOutlineMail} from "react-icons/ai"
+import {AiOutlineHome, AiOutlineTwitter, AiOutlineClose, AiOutlineInstagram, AiFillGithub, AiOutlineMail} from "react-icons/ai"
 import {VscDebugBreakpointDataUnverified, VscDebugBreakpointFunction} from "react-icons/vsc"
 import { useState } from "react";
+import {FaTiktok} from "react-icons/fa"
 function App() {
   const [display, notDisplay] = useState();
   return (
@@ -31,9 +32,9 @@ function App() {
       </>
     }
   </div>
-  <MainButton additional_clases="animate__animated animate__fadeInUp" className="main-btn" text="Más información" goto="#second-section"/>
+  <MainButton additional_clases="animate__animated animate__fadeInUp" className="main-btn" text="Más información" goto="#projects"/>
   </section>
-  <section id="second-section" className="second-section">
+  <section id="projects" className="second-section">
    <h2 className="subsection-title">{<VscDebugBreakpointDataUnverified/>} Proyectos:</h2> 
    <ul className="project-list">
     <li>
@@ -48,14 +49,23 @@ function App() {
         Mis trabajos para este proyecto han sido referenciados en medios como <b>ABC de Sevilla</b>, <b>El Español</b> y <b>En Castilla la Mancha</b> entre otros; así como también en redes sociales oficiales de distinas instituciones.
         <br/>
         <br/>
-        <b>Referencias en medios:</b>
+        <b className="white">Referencias en medios:</b>
         <Reference media="ABC" text="La policía local de Sevilla, protagonista en el GTA V" goto="https://sevilla.abc.es/tecnologia/videojuegos/sevi-policia-local-sevilla-protagonista-202007151106_noticia.html"/>
         <Reference media="El Español" text="La Policía Foral, en el videojuego más vendido de la historia" goto="https://navarra.elespanol.com/articulo/sociedad/gtav-mods-forales-municipales-pamplona/20201222170514349854.html"/> 
         <Reference media="ECM" text="La Policía Local de Toledo, en el GTA V" goto="https://www.encastillalamancha.es/castilla-la-mancha-cat/toledo/la-policia-local-de-toledo-en-el-gta-v-realismo-en-el-famoso-videojuego-con-aviso-por-covid-persecucion/"/>
-        <b>Referencias en redes sociales:</b>
+        <b className="white">Referencias en redes sociales:</b>
         <Reference text="Twitter de la Ertzaintza" goto="https://twitter.com/ertzaintzaEJGV/status/1462399103296540678"/>
         <Reference text="Twitter de la policía local de Alcorcón" goto="https://twitter.com/PoliciaAlcorcon/status/1572255413860536320"/>
+        <br/>
+        <b className="white"><u>Redes sociales y enlaces a Mmods GTAV:</u></b>
+          <div className="rrss-box-holder">
+          <Box icon={<AiOutlineHome/>} text="Web" goto="https://mmodsgtav.es"/>
+          <Box icon={<AiOutlineTwitter/>} text="Twitter" goto="https://twitter.com/mmodsgtav"/>
+          <Box icon={<AiOutlineInstagram/>} text="Instagram" goto="https://instagram.com/mmodsgtav"/>
+          <Box icon={<FaTiktok/>} text="TikTok" goto="https://tiktok.com/@mmodsgtav"/>
+          </div>
       </p>
+      <hr/>
       </div>
       <div className="logo-holder">
         <img className="mmodsgtav-logo" src="https://mmodsgtav.es/cdn/img/logo.png"/>
@@ -63,6 +73,44 @@ function App() {
       </div>
       </li>
 
+   </ul>
+   <h2 className="subsection-title">{<VscDebugBreakpointDataUnverified/>} Formación:</h2> 
+   <ul className="project-list">
+    <li>
+      <div className="text-holder">
+   <p className="mobile-text">
+    Actualmente estoy estudiando segundo año de Ingeniería Informática en la UPV/EHU.
+   </p>
+   <hr/>
+   </div>
+   </li>
+   </ul>
+   <h2 className="subsection-title">{<VscDebugBreakpointDataUnverified/>} Experiencia:</h2> 
+   <ul className="project-list">
+    <li>
+      <div className="text-holder">
+   <p className="mobile-text">
+    Experiencia demostrable en el ámbito del desarrollo web. Conocimientos en <b>PHP</b>, <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b> y frameworks JS como <b>React</b> o <b>NextJS</b>; así como experiencia en el manejo de bases de datos relacionales como <b>MySQL</b> y en el desarrollo de API Rests.
+    <br/>
+    También tengo experiencia en <b>Python</b> desarrollando bots para Discord y en otras plataformas como .NET de Microsoft. 
+   </p>
+   <hr/>
+   </div>
+   </li>
+   </ul>
+
+   <h2 className="subsection-title">{<VscDebugBreakpointDataUnverified/>} Contacto:</h2> 
+   <ul className="project-list">
+    <li>
+      <div className="text-holder">
+      <div className="rrss-box-holder">
+          <Box icon={<AiOutlineTwitter/>} text="Twitter" goto="https://twitter.com/mikelmiras_"/>
+          <Box icon={<AiOutlineInstagram/>} text="Instagram" goto="https://instagram.com/mikemiras_"/>
+          <Box icon={<AiFillGithub/>} text="Github" goto="https://github.com/mikelmiras"/>
+          <Box icon={<AiOutlineMail/>} text="Email" goto="mailto:m@mikelmiras.com"/>
+          </div>
+   </div>
+   </li>
    </ul>
   </section>
   </>    
@@ -78,14 +126,28 @@ function Reference(props){
 if (props.media){
   return(
     <>
-    <a target="_blank" className="reference-a" href={props.goto}><p className="reference">{<VscDebugBreakpointFunction/>} <span>{props.text}, {props.media}</span></p></a>
+    <a target="_blank" className="reference-a" href={props.goto}><p className="reference">{<VscDebugBreakpointFunction className="white"/>} <span>{props.text}, {props.media}</span></p></a>
     </>
   )
 }else{
   return(
     <>
-    <a target="_blank" className="reference-a" href={props.goto}><p className="reference">{<AiOutlineTwitter/>} <span>{props.text}</span></p></a>
+    <a target="_blank" className="reference-a" href={props.goto}><p className="reference">{<AiOutlineTwitter className="white"/>} <span>{props.text}</span></p></a>
     </>
   )
 }
+}
+
+function Box(props){
+  return(
+    <>
+    <div className="rrss-box">
+      <a target="_blank" href={props.goto}>
+        <p>
+          {props.icon} {props.text} 
+        </p>
+      </a>
+    </div>
+    </>
+  )
 }
