83d1860d08a41bcd55b5a989cd4c116bd24fadde
Mikel <mmiras004@ikasle.ehu.eus>
2023-02-22 01:52:41 +0100

    Animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/App.css"
 import "../styles/Mobile.css"
 import "../styles/Pc.css"
+import 'animate.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
