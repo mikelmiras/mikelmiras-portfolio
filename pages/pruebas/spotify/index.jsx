f4a37d711aadb139781ad59ddee263c28b5d47b7
Mikel <mmiras004@ikasle.ehu.eus>
2023-03-10 13:16:34 +0100

    params fix

--- pages/pruebas/spotify/index.jsx
+++ pages/pruebas/spotify/index.jsx
@@ -1,7 +1,7 @@
 export default function SpotiPrueba(params){
     return(
         <>
-         <a href={"https://accounts.spotify.com/es-ES/authorize?client_id=641e281228a54c61a8f4095778f2df49&redirect_uri=" + url + "&response_type=code&scope=user-top-read"}>Login with Spotify</a>
+         <a href={"https://accounts.spotify.com/es-ES/authorize?client_id=641e281228a54c61a8f4095778f2df49&redirect_uri=" + params.url + "&response_type=code&scope=user-top-read"}>Login with Spotify</a>
         </>
     )
 }
