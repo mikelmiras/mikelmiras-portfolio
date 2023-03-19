38b87d8a6bdd65c8ebcfef3c7054ed6a62d1b7ad
mikelmiras <mmiras004@ikasle.ehu.eus>
2023-03-19 22:30:59 +0100

    Contacto prueba

---
+++ pages/contacto.jsx
@@ -0,0 +1,9 @@
+export default function Contacto() {
+    return(
+        <>
+        <section className="main-info-holder">
+            <h1>Esta es la página de contacto</h1>
+        </section>
+        </>
+    )
+}
